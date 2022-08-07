2547eabbaa348f948a5d387de16453317fda3e2f
DESKTOP-5KSGJN4\gywkd <gywkd10@naver.com>
2022-08-07 15:22:48 +0900

    두번째 실습 시작

--- Project1_1/Project1_1/main.cpp
+++ Project1_1/Project1_1/main.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main(void)
 {
+	int currentHP = 100;
+
+	currentHP = 50;
+	cout << "현재 내 체력 : " << currentHP << endl;
+
+	int a = 10;
+	int b = 20;
+
+	cout << "a + b = " << a + b << endl;
 
 	return 0;
 }
